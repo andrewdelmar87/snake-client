f3aa50350de52cb017a6944abcdbe4880b83fcda
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-03 21:04:32 +0000

    Refactored

--- client.js
+++ client.js
@@ -22,6 +22,6 @@ const connect = function() {
   });
 
   return conn;
+});
 };
-
 module.exports = {connect};
--- play.js
+++ play.js
@@ -4,3 +4,21 @@ const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+setupInput();
+
+const handleUserInput = function() {
+  setupInput().on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+handleUserInput();
