9f181ee653e4b6d63d97c690a5bd477b6a8cf2e1
Andrew Delmar <andrewjdelmar@gmail.com>
2020-06-03 20:06:38 +0000

    Refactored

--- play.js
+++ play.js
@@ -1,30 +1,6 @@
 const net = require('net');
 
-const stdin = process.stdin;
-stdin.setRawMode(true);
-stdin.setEncoding('utf8');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server');
-  });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-  
-  return conn;
-};
+const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
